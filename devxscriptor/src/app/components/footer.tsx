2a57460d707a0fb2e85e480f727ecc77b5426676
xscriptor <preciado.oscar.osorio@gmail.com>
2026-04-04 13:06:07 +0200

    update repos and x page on dev

--- devxscriptor/src/app/components/footer.tsx
+++ devxscriptor/src/app/components/footer.tsx
@@ -3,7 +3,7 @@ import { PagesStyles } from "./pagesstyles";
 export default function Footer() {
   return (
     <footer className={`${PagesStyles.footerStyles}`}>
-      <h3>Xscriptor/Code &copy;</h3>
+      <h3>Xscriptor/Dev &copy;</h3>
     </footer>
   );
 }
