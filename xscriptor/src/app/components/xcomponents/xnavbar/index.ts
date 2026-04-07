fb8f0b76d61bb1f04f97e52354db3b48f7d19325
xscriptor <preciado.oscar.osorio@gmail.com>
2026-04-07 20:35:46 +0200

    Update navbar component - change to XNavbarComponent

---
+++ xscriptor/src/app/components/xcomponents/xnavbar/index.ts
@@ -0,0 +1,3 @@
+// Barrel export – importa desde aquí en lugar de apuntar al archivo directamente
+export { default } from "./XNavbar";
+export type { XNavbarProps, NavLinkItem, ThemeToggleIcons } from "./XNavbar";
